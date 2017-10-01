92c58e325b4a1506079ebecdd2cf21358787bee1
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-10-01 09:34:34 +0700

    Use cos. instead of cos

--- plotting/test_pyplot.jl
+++ plotting/test_pyplot.jl
@@ -1,7 +1,7 @@
 using PyPlot
 
 x = linspace(0, 2*pi, 1000)
-y = cos(3.0*x.^2 + 4*cos(2*x))
+y = cos.(3.0*x.^2 + 4*cos(2*x))
 plot(x, y, color="red", linewidth=2.0, linestyle="--")
 title("A sinusoidally modulated sinusoid")
 savefig("CosCos.png", dpi=300)
