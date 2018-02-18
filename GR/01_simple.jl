ecd97bb00dce13abc6bf73572394223ea87abc0e
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-02-18 09:30:58 +0700

    Add GR

---
+++ GR/01_simple.jl
@@ -0,0 +1,13 @@
+import GR
+
+function test_main()
+    Npoints = 50
+    x = Array(linspace(0.0, 1.0, 50))
+    y = zeros(Npoints)
+    for i = 1:Npoints
+        y[i] = sin(x[i])
+    end
+    GR.plot(x, y)
+end
+
+test_main()
