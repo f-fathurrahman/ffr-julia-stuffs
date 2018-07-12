1536e8ae55f15dbd53d0b6217922fd54655426dd
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-07-12 09:54:07 +0700

    Start preparing MANUAL_*, for exploring Julia's manual

---
+++ MANUAL_multidim_arrays/01_construction.jl
@@ -0,0 +1,28 @@
+using Printf
+
+struct MyType
+    x::Int
+    y::Int
+end # struct MyType
+
+
+"""
+Explore how the initial values of an array initialized with `undef`
+with various data type.
+"""
+function test01()
+    
+    # Built-in types
+    for typ in (Int32, Int64, Float64, ComplexF64, Bool)
+        println("\nType = ", typ)
+        A = Array{typ}(undef,3,3)
+        println(A)
+    end
+
+    # Using user-defined type
+    println("\nType = ", MyType)
+    A = Array{MyType}(undef,3,3)
+    println(A)
+end
+
+test01()
