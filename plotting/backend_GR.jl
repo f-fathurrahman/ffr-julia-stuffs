4a220a24119a3774dbb24c53a91cf1e7dc2ece36
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-08-26 21:03:00 +0700

    Lets try again Plots.jl, moving pyplot scripts to their own subdir

--- plotting/backend_GR.jl
+++ plotting/backend_GR.jl
@@ -1,12 +1,9 @@
 using Plots
 gr()
 
-#plot( Plots.fakedata(50,5), w=3 )
-#savefig( "ex1.pdf" )
-
 p = plot( [sin,cos], zeros(0), leg=false )
 anim = Animation()
-for x = linspace(0,10π,100)
+for x = range(0,stop=10π,length=100)
   push!( p, x, Float64[sin(x), cos(x)] )
   frame(anim)
 end
