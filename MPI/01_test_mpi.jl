e34146f023d9740c0d2e35203c54640313ab4d3e
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-08-28 11:52:02 +0700

    Update 01_test_mpi

--- MPI/01_test_mpi.jl
+++ MPI/01_test_mpi.jl
@@ -1,22 +1,34 @@
+using Statistics
+
 import MPI
 
 function main()
     MPI.Init()
     comm = MPI.COMM_WORLD
-    size = MPI.Comm_size( comm )
-    rank = MPI.Comm_rank( comm )
+
+    Nprocs  = MPI.Comm_size( comm )
+    my_rank = MPI.Comm_rank( comm )
+
+    println("Nprocs  = ", Nprocs)
+    println("my_rank = ", my_rank)
+
     recv_mesg = zeros(100,1)
-    if rank == 0
-        send_mesg = rand(100,1)
-        for i = 1:size-1
+
+    if my_rank == 0
+        send_mesg = ones(100,1)
+        for i = 1:Nprocs-1
             MPI.Send( send_mesg, i, 0, comm )
         end
     else
         MPI.Recv!( recv_mesg, 0, 0, comm )
     end
-    m = mean( recv_mesg, 1 )
-    sleep( rank*2 )
-    println("Results of rank ", rank, ": ", m)
+    
+    m = mean( recv_mesg, dims=1 )
+    
+    #sleep( my_rank*2 )
+    
+    println("Results of rank ", my_rank, ": ", m)
+    
     MPI.Barrier( comm )
     MPI.Finalize()
 end
