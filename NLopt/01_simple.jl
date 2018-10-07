067c915bc146dab9f7bc262e178607fbe03004cf
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-10-07 08:01:49 +0700

    Add NLopt example

---
+++ NLopt/01_simple.jl
@@ -0,0 +1,29 @@
+using NLopt
+
+function myfunc(x::Vector, grad::Vector)
+    if length(grad) > 0
+        grad[1] = 0
+        grad[2] = 0.5/sqrt(x[2])
+    end
+    return sqrt(x[2])
+end
+
+function myconstraint(x::Vector, grad::Vector, a, b)
+    if length(grad) > 0
+        grad[1] = 3a * (a*x[1] + b)^2
+        grad[2] = -1
+    end
+    (a*x[1] + b)^3 - x[2]
+end
+
+opt = Opt(:LD_MMA, 2)
+lower_bounds!(opt, [-Inf, 0.])
+xtol_rel!(opt,1e-4)
+
+min_objective!(opt, myfunc)
+inequality_constraint!(opt, (x,g) -> myconstraint(x,g,2,0), 1e-8)
+inequality_constraint!(opt, (x,g) -> myconstraint(x,g,-1,1), 1e-8)
+
+(minf,minx,ret) = optimize(opt, [1.234, 5.678])
+numevals = opt.numevals # the number of function evaluations
+println("got $minf at $minx after $numevals iterations (returned $ret)")
