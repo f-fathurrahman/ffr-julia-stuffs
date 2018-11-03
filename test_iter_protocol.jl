53eb31ae0aacd06b8b242e3a44a86b7a2a978c1e
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-11-03 13:39:08 +0700

    Fix to version 1.0

--- test_iter_protocol.jl
+++ test_iter_protocol.jl
@@ -1,11 +1,10 @@
 function f(iter)
-    state = start(iter)
-    i = 0
-    while !(done(iter, state)) & (i < 1_000_000)
-       i += 1
-       v, state = next(iter, state)
+    next = iterate(iter)
+    while next !== nothing
+       (i, state) = next
+       next = iterate(iter, state)
     end
 end
 
-@time f(zeros(1_000_000))
-@time f(zeros(1_000_000))
+@time f(1:1_000_000)
+@time f(1:1_000_000)
