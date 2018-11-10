71934863589b3aab4651061da643d6174a69e1ac
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-11-10 23:04:44 +0700

    Update for julia-1.0.x

--- spectral_methods/discrete_fourier_coefficients.jl
+++ spectral_methods/discrete_fourier_coefficients.jl
@@ -3,23 +3,23 @@
 #
 function discrete_fourier_coefficients( f )
 
-  N = size(f)[1]
-  F = zeros( Complex128, N )
+    N = size(f)[1]
+    F = zeros(ComplexF64, N)
 
-  #for k = -N/2 : N/2
-  for kk = 1:N
-    k = nn_to_mm(kk,N)
-    s = 0.0 + im*0.0
-    for j = 0:N-1
-      s = s + f[j+1]*exp(-2*pi*im*j*k/N)
+    #for k = -N/2 : N/2
+    for kk = 1:N
+        k = nn_to_mm(kk,N)
+        s = 0.0 + im*0.0
+        for j = 0:N-1
+            s = s + f[j+1]*exp(-2*pi*im*j*k/N)
+        end
+        F[kk] = s/N
     end
-    F[kk] = s/N
-  end
-  return F
+    return F
 end
 
 # map k = [-N/2:N/2] to 1:N
 # FIXME: Need to to think of another name for this function
-function nn_to_mm( nn::Int, S::Int )
-  return nn - Int(floor(S/2)) - 1
+function nn_to_mm( nn::Int64, S::Int64 )
+    return nn - Int64(floor(S/2)) - 1
 end
--- spectral_methods/ex_discrete_fourier_coefficients.jl
+++ spectral_methods/ex_discrete_fourier_coefficients.jl
@@ -1,30 +1,34 @@
+using Printf
+using Random
+using FFTW
+
 include("discrete_fourier_coefficients.jl")
 
-function test_nn_mm()
-  N = 4
-  a = range(1,N)
-  for i in a
-    A = nn_to_mm(i,N)
-    println(A)
-  end
+function test_nn_mm(N::Int64)
+    for i = 1:N
+        A = nn_to_mm(i,N)
+        println(A)
+    end
 end
 
 function test_main()
 
-  N = 5
+    N = 5
+    test_nn_mm(N)
 
-  #f = ones(N)
-  srand(1234)
-  f = rand(N)
+    #f = ones(N)
+    Random.seed!(1234)
+    f = rand(N)
 
-  F = discrete_fourier_coefficients(f)
+    F = discrete_fourier_coefficients(f)
 
-  F_fft = fft(f)/N  # need normalization to match the convention used here
+    F_fft = fft(f)/N  # need normalization to match the convention used here
 
-  for i = 1:N
-    @printf("%4d %18.10f (%18.10f,%18.10f) (%18.10f,%18.10f)\n",
-            i, f[i], real(F[i]), imag(F[i]), real(F_fft[i]), imag(F_fft[i]) )
-  end
+    # comparing the result between fft and discrete_fourier_coefficients
+    for i = 1:N
+        @printf("%4d %18.10f (%18.10f,%18.10f) (%18.10f,%18.10f)\n",
+                i, f[i], real(F[i]), imag(F[i]), real(F_fft[i]), imag(F_fft[i]) )
+    end
 end
 
 test_main()
