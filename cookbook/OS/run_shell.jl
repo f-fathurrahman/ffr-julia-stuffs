03775cdb8b6647f20697c699505754559ef8e036
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-06-08 16:49:29 +0700

    Display command

--- cookbook/OS/run_shell.jl
+++ cookbook/OS/run_shell.jl
@@ -2,8 +2,6 @@ my_cmd = `cat identify_os_type.jl`
 
 println("typeof my_cmd: ", typeof(my_cmd))
 
-println()
-println("Running cat to show the content of identify_os_type.jl")
-println()
-run(`cat identify_os_type.jl`)
+println("I will run: ", my_cmd)
+run(my_cmd)
 
