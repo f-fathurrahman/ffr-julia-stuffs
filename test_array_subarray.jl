95ae8fe837c8208ed9d16781fcbdd09413fc4ff2
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2021-08-03 14:48:57 +0700

    Test using AbstractArray

--- test_array_subarray.jl
+++ test_array_subarray.jl
@@ -1,6 +1,11 @@
+using BenchmarkTools
+
 #function my_func1!(A::Array{Float64,2})
 #function my_func1!( A::Array{Float64} )
-function my_func1!( A )
+#function my_func1!( A )
+#function my_func1!( A::AbstractArray{T} ) where {T<:Float64}
+#function my_func1!( A::AbstractArray{Float64} )
+function my_func1!( A::AbstractArray{Float64,2} )
     Nrow = size(A,1)
     Ncol = size(A,2)
     for j in 1:Ncol
@@ -15,8 +20,7 @@ function main()
     A = rand(5,5)
     my_func1!(A)
     @views my_func1!(A[1:3,1:2])
-    display(A); println()
 end
 
-main()
+@btime main()
 
