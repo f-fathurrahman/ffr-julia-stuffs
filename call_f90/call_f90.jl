49d02fca3a0d82961bad755be167f7ae3423d965
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2022-03-30 00:06:10 +0700

    Revisit call_f90

--- call_f90/call_f90.jl
+++ call_f90/call_f90.jl
@@ -1,7 +1,7 @@
 # compile the module with
 # gfortran -fPIC -shared -O2 module1.f90 -o module1.so
 
-ccall( (:__mymodule_MOD_init_mymodule, "module1.so"), Void, () )
+ccall( (:__mymodule_MOD_init_mymodule, "module1.so"), Cvoid, () )
 
 mm = ccall( (:__mymodule_MOD_five, "module1.so"), Int, () )
 println(mm)
@@ -15,5 +15,9 @@ nn = ccall( (:__mymodule_MOD_get_gbl_r, "module1.so"), Float64, () )
 println(nn)
 
 Ptr_gbl_Arr1 = cglobal( (:__mymodule_MOD_gbl_arr1, "module1.so"), Float64 )
-gbl_Arr1 = unsafe_load(Ptr_gbl_Arr1,3)
+gbl_Arr1 = unsafe_load(Ptr_gbl_Arr1,3) # only 3rd index is loaded
 println(gbl_Arr1)
+
+Ptr_gbl_Arr2 = cglobal( (:__mymodule_MOD_gbl_arr2, "module1.so"), Float64 )
+gbl_Arr2 = unsafe_wrap(Array{Float64,2}, Ptr_gbl_Arr2, (4,3))
+println(gbl_Arr2)
--- call_f90/module1.f90
+++ call_f90/module1.f90
@@ -5,6 +5,7 @@ IMPLICIT NONE
 INTEGER, PARAMETER :: MYPARAM1 = 4
 REAL(8) :: gbl_R = 8.d0
 REAL(8) :: gbl_Arr1(3)
+REAL(8) :: gbl_Arr2(4,3)
 
 TYPE MyType_T
   INTEGER :: Ndata
@@ -17,6 +18,10 @@ CONTAINS
 
 SUBROUTINE init_MyModule()
   gbl_Arr1 = (/ 1.1d0, 2.2d0, 3.4d0 /)
+  gbl_Arr2(1,:) = (/ 1.1d0, 2.2d0, 3.4d0 /)
+  gbl_Arr2(2,:) = (/ 2.1d0, 2.3d0, 4.4d0 /)
+  gbl_Arr2(3,:) = (/ 3.1d0, 2.4d0, 5.4d0 /)
+  gbl_Arr2(4,:) = (/ 4.1d0, 2.5d0, 6.4d0 /)
 END SUBROUTINE
 
 INTEGER FUNCTION five()
