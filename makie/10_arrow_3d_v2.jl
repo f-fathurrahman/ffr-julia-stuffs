d6fe469f4c0b5cd2a92fed81aed099d7e19ed916
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2023-08-03 07:03:39 +0700

    Test theme_dark, not good

---
+++ makie/10_arrow_3d_v2.jl
@@ -0,0 +1,15 @@
+# https://makie.juliaplots.org/v0.15.2/examples/plotting_functions/arrows/
+
+# update_theme!(theme_dark()) does not change the default color of arrows
+
+using GLMakie
+
+ps = [Point3f(x, y, z) for x in -5:2:5 for y in -5:2:5 for z in -5:2:5]
+ns = map(p -> 0.1 * Vec3f(p[2], p[3], p[1]), ps)
+arrows(
+    ps, ns, fxaa=true, # turn on anti-aliasing
+    linecolor = :blue, arrowcolor = :red,
+    linewidth = 0.1, arrowsize = Vec3f(0.3, 0.3, 0.4),
+    align = :center, axis=(type=Axis3,)
+)
+
