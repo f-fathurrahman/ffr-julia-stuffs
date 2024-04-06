2bc6cb216e382784c9559b4ae7935f3bbab67a90
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2024-04-07 01:04:36 +0700

    Test code gen 01, parse(String) -> Expr -> eval

---
+++ test_code_gen_01.jl
@@ -0,0 +1,27 @@
+function get_var1()
+    return rand(3)
+end
+
+function get_var2()
+    return randn(7)
+end
+
+function build_expr_prog1(v)
+    str_prog = """$v = get_$v()"""
+    return Meta.parse(str_prog)
+end
+
+function build_expr_prog2(v)
+    str_prog = """println("$v = ", $v)"""
+    return Meta.parse(str_prog)
+end
+
+function main()
+    for v in (:var1, :var2)
+        eval(build_expr_prog1(v))
+        eval(build_expr_prog2(v))
+    end
+end
+
+main()
+
